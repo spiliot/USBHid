5a4ab0eca83fa62f09eb002ecafabf20cee91d47
spiliot <spiliot@gmail.com>
2017-04-04 03:28:57 +0300

    Adds a class to hold the description strings

---
+++ USB/Classes/UsbDescriptorStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UsbHid.USB.Classes
+{
+    public class UsbDescriptorStrings
+    {
+        public readonly string Manufacturer;
+        public readonly string Product;
+        public readonly string Serial;
+
+        public UsbDescriptorStrings(string Manufacturer, string Product, string Serial)
+        {
+            this.Manufacturer = Manufacturer;
+            this.Product = Product;
+            this.Serial = Serial;
+        }
+    }
+}
